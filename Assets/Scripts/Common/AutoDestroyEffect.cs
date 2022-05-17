98bd8d9c9a785433a1b07ab877405e91def6647a
mol1986@naver.com <kileab@naver.com>
2022-05-17 17:58:09 +0900

    colltion rate

--- Assets/Scripts/Common/AutoDestroyEffect.cs
+++ Assets/Scripts/Common/AutoDestroyEffect.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 // 해당 컴포넌트가 붙어 있어야 함을 강제한다.
 [RequireComponent(typeof(ParticleSystem))]
-public class AudoDestroyEffect : MonoBehaviour
+public class AutoDestroyEffect : MonoBehaviour
 {
     ParticleSystem particle;
     private void Start()
